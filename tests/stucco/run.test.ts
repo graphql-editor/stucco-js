e7da8ea21f521a5bdaf2be7137ed9e12044e8134
Kamil Matysiewicz <kamil@aexol.com>
2019-11-29 12:50:18 +0100

    test: be a bit more permissive with stucco binary fetching

--- tests/stucco/run.test.ts
+++ tests/stucco/run.test.ts
@@ -2,7 +2,11 @@ import { execFileSync } from "child_process";
 import { stucco } from "../../src/stucco/run";
 import { version } from "../../src/stucco/version";
 
-test("fetch correct stucco", async () => {
-  const bin = await stucco();
-  expect(execFileSync(bin.path(), ["-version"]).toString().trim()).toEqual(version);
+describe("stucco", () => {
+  // file downlaod, be a bit more permissive
+  jest.setTimeout(10000);
+  test("fetch correct version", async () => {
+    const bin = await stucco();
+    expect(execFileSync(bin.path(), ["-version"]).toString().trim()).toEqual(version);
+  });
 });
