c2ab2696f68d42dd028f05677e26d119d8441ef6
Kamil Matysiewicz <kamil@aexol.com>
2019-12-18 14:22:50 +0100

    fix: linter errors

--- src/server/server.ts
+++ src/server/server.ts
@@ -34,7 +34,6 @@ import {
   FieldResolveInfo,
   FieldResolveRequest,
   FieldResolveResponse,
-  Function as DriverFunction,
   InterfaceResolveTypeRequest,
   InterfaceResolveTypeResponse,
   ObjectValue,
