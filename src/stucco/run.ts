019e59adab8739212757b0c98ab2ddf8226d3b95
Kamil Matysiewicz <kamil@graphqleditor.com>
2021-09-21 11:12:36 +0200

    Add retry for invalid stucco binary version

--- src/stucco/run.ts
+++ src/stucco/run.ts
@@ -2,18 +2,23 @@
 
 import BinWrapper from 'bin-wrapper';
 import { spawn } from 'child_process';
-import * as path from 'path';
 import { version } from './version';
 import binCheck from 'bin-check';
 import binVersionCheck from 'bin-version-check';
+import { retry } from '../util/util';
+import { rmdir } from 'fs';
+import { promisify } from 'util';
+import { join } from 'path';
+const rmdirP = promisify(rmdir);
 
 const base = 'https://stucco-release.fra1.cdn.digitaloceanspaces.com';
-export const stucco = async (): Promise<BinWrapper> => {
+const dest = join(__dirname, 'vendor');
+async function fetchCheckBin(): Promise<BinWrapper> {
   const bin = new BinWrapper({ skipCheck: true })
     .src(`${base}/${version}/darwin/amd64/stucco`, 'darwin')
     .src(`${base}/${version}/linux/amd64/stucco`, 'linux', 'x64')
     .src(`${base}/${version}/windows/amd64/stucco.exe`, 'win32', 'x64')
-    .dest(path.join(__dirname, 'vendor'))
+    .dest(dest)
     .use(process.platform === 'win32' ? 'stucco.exe' : 'stucco');
   await bin.run();
   const works = await binCheck(bin.path());
@@ -22,7 +27,20 @@ export const stucco = async (): Promise<BinWrapper> => {
   }
   await binVersionCheck(bin.path(), `~${version}`, { args: ['version'] });
   return bin;
-};
+}
+
+async function tryRemove(e: unknown): Promise<void> {
+  await rmdirP(dest, { recursive: true });
+  throw e;
+}
+
+export async function stucco(): Promise<BinWrapper> {
+  const bin = await retry(() => fetchCheckBin().catch(tryRemove), { retries: 2, minTimeout: 10 });
+  if (!bin) {
+    throw new Error('could not find stucco binary');
+  }
+  return bin;
+}
 
 if (require.main === module) {
   (async (): Promise<void> => {
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,29 @@
+import { operation, OperationOptions } from 'retry';
+
 export function notUndefined<T>(v: T | undefined): v is T {
   return v !== undefined;
+}
+
+export async function retry<T>(f: () => Promise<T> | T, opts?: OperationOptions): Promise<T> {
+  const op = operation(
+    opts || {
+      retries: 5,
+      factor: 3,
+      minTimeout: 1 * 1000,
+      maxTimeout: 60 * 1000,
+      randomize: true,
+    },
+  );
+  return new Promise<T>((resolve, reject) =>
+    op.attempt(async () => {
+      try {
+        resolve(await f());
+      } catch (e) {
+        const err = e === undefined || e instanceof Error ? e : new Error('unknown error');
+        if (!op.retry(err)) {
+          reject(err);
+        }
+      }
+    }),
+  );
 }
