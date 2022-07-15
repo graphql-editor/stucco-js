2b3c98407dba78a3eb092947345b4386dd6f9e7e
Kamil Matysiewicz <kamil@aexol.com>
2022-07-15 14:45:14 +0200

    Expand http protocol on input

--- src/proto/driver/protocol.ts
+++ src/proto/driver/protocol.ts
@@ -1,6 +1,40 @@
-import { HttpRequest } from '../../api/index.js';
-import { RecordOfUnknown, getFromValue } from './value.js';
+import { HttpRequest, HttpRequestURL } from '../../api/index.js';
+import { getFromValue, RecordOfUnknown } from './value.js';
 import * as messages from './messages.js';
+
+type Headers = Record<string, string[]>;
+
+const newOptionalCheck = <T>(check: (v: unknown) => v is T) => (v: unknown | undefined | null): v is T | undefined | null => v === undefined || v === null || check(v);
+
+const checkOptionalUint8Array = newOptionalCheck((v: unknown): v is Uint8Array => v instanceof Uint8Array);
+const checkOptionalString = newOptionalCheck((v: unknown): v is string => typeof v === 'string');
+const checkOptionalHeaders = newOptionalCheck((v: unknown): v is Headers => {
+  if (typeof v !== 'object' || v === null) return false;
+  const nv = v as RecordOfUnknown;
+  return (
+    Object.keys(nv).find((k) => {
+      const header = nv[k];
+      if (!Array.isArray(header)) {
+        return true;
+      }
+      return header.find((el) => typeof el !== 'string');
+    }) === undefined
+  );
+});
+
+function isHttpRequestURL(url: unknown): url is HttpRequestURL {
+  if (typeof url !== 'object') {
+    return false;
+  }
+  const { host, path, query } = (url as {
+    host?: unknown;
+    path?: unknown;
+    query?: unknown;
+  });
+  return checkOptionalString(host) && checkOptionalString(path) && checkOptionalString(query);
+}
+
+const checkOptionalHttpRequestURL = newOptionalCheck((v: unknown): v is HttpRequestURL => isHttpRequestURL(v));
 
 function isHttpRequestProtocol(protocol: unknown): protocol is HttpRequest {
   if (typeof protocol !== 'object' || protocol === null) {
@@ -9,20 +43,34 @@ function isHttpRequestProtocol(protocol: unknown): protocol is HttpRequest {
   if (!('headers' in protocol)) {
     return false;
   }
-  const unknownHeaders = (protocol as { headers: unknown }).headers;
-  if (typeof unknownHeaders !== 'object' || unknownHeaders === null) {
+  const {
+    headers,
+    body,
+    host,
+    method,
+    proto,
+    remoteAddress,
+    url,
+  } = (protocol as {
+    headers?: unknown;
+    body?: unknown;
+    host?: unknown;
+    method?: unknown;
+    proto?: unknown;
+    remoteAddress?: unknown;
+    url?: unknown;
+  });
+  // If none of the properties are present, return false
+  if (!headers && !body && !host && !method && !proto && !remoteAddress && !url) {
     return false;
   }
-  const headers = unknownHeaders as RecordOfUnknown;
-  return (
-    Object.keys(headers).find((k) => {
-      const header = headers[k];
-      if (!Array.isArray(header)) {
-        return true;
-      }
-      return header.find((el) => typeof el !== 'string');
-    }) === undefined
-  );
+  return checkOptionalUint8Array(body) ||
+    checkOptionalString(host) ||
+    checkOptionalString(method) ||
+    checkOptionalString(proto) ||
+    checkOptionalString(remoteAddress) ||
+    checkOptionalHttpRequestURL(url) ||
+    checkOptionalHeaders(headers);
 }
 
 interface WithProtocol {
