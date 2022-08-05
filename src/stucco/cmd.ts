1f30139c66027b1d71ec5b10e29a8feb91d89449
Kamil Matysiewicz <kamil@graphqleditor.com>
2022-08-05 16:02:50 +0200

    [chore] Refactor spawn stdio

--- src/stucco/cmd.ts
+++ src/stucco/cmd.ts
@@ -4,7 +4,7 @@ import { spawn } from 'child_process';
 (async (): Promise<void> => {
   const bin = await stucco();
   const args = process.argv.length > 2 ? process.argv.slice(2) : ['local', 'start'];
-  const child = spawn(bin.path(), args, { stdio: [process.stdin, process.stdout, process.stderr] });
+  const child = spawn(bin.path(), args, { stdio: 'inherit' });
   const kill = () => child.kill();
   process.on('SIGTERM', kill);
   process.on('SIGINT', kill);
