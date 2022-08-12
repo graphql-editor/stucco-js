7fe1deb01baaa715c12ceade56cc7c479da2d1bb
Kamil Matysiewicz <kamil@graphqleditor.com>
2022-08-12 14:26:51 +0200

    Cleanup dependencies

--- src/stucco/cmd.ts
+++ src/stucco/cmd.ts
@@ -4,7 +4,7 @@ import { spawn } from 'child_process';
 (async (): Promise<void> => {
   const bin = await stucco();
   const args = process.argv.length > 2 ? process.argv.slice(2) : ['local', 'start'];
-  const child = spawn(bin.path(), args, { stdio: 'inherit' });
+  const child = spawn(bin, args, { stdio: 'inherit' });
   const kill = () => child.kill();
   process.on('SIGTERM', kill);
   process.on('SIGINT', kill);
--- src/stucco/run.ts
+++ src/stucco/run.ts
@@ -1,45 +1,83 @@
 #!/usr/bin/env node
 
-import BinWrapper from 'bin-wrapper';
 import { version } from './version.js';
-import binCheck from 'bin-check';
-import binVersionCheck from 'bin-version-check';
-import { retry } from '../util/util.js';
-import { rmdir } from 'fs';
-import { promisify } from 'util';
+import { platform, arch } from 'os';
+import { promises, createWriteStream } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { get } from 'https';
+import { retry } from '../util/util.js';
+import { IncomingMessage } from 'http';
+import binVersionCheck from 'bin-version-check';
+const { chmod, rm, stat, mkdir } = promises;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const rmdirP = promisify(rmdir);
-
-const base = 'https://stucco-release.fra1.cdn.digitaloceanspaces.com';
+const base = `https://stucco-release.fra1.cdn.digitaloceanspaces.com/${version}`;
 const dest = join(__dirname, 'vendor');
-async function fetchCheckBin(): Promise<BinWrapper> {
-  const bin = new BinWrapper({ skipCheck: true })
-    .src(`${base}/${version}/darwin/amd64/stucco`, 'darwin')
-    .src(`${base}/${version}/linux/amd64/stucco`, 'linux', 'x64')
-    .src(`${base}/${version}/windows/amd64/stucco.exe`, 'win32', 'x64')
-    .dest(dest)
-    .use(process.platform === 'win32' ? 'stucco.exe' : 'stucco');
-  await bin.run();
-  const works = await binCheck(bin.path());
-  if (!works) {
-    throw new Error(`binary ${bin.path()} does not seem to work`);
+const filename = join(dest, platform() === 'win32' ? 'stucco.exe' : 'stucco');
+const src = () => {
+  const src: Partial<Record<NodeJS.Platform, Record<string, string>>> = {
+    darwin: {
+      x64: `${base}/darwin/amd64/stucco`,
+    },
+    linux: {
+      x64: `${base}/linux/amd64/stucco`,
+    },
+    win32: {
+      x64: `${base}/windows/amd64/stucco`,
+    },
+  };
+  const plat = src[platform()] || {};
+  return plat[arch()];
+};
+
+const fetchBin = async () => {
+  const res = await new Promise<IncomingMessage>((resolve, reject) => {
+    const req = get(src(), resolve);
+    req.on('error', reject);
+  });
+  const { statusCode = 0 } = res;
+  if (statusCode < 200 || statusCode > 299) {
+    throw new Error("Did not recieve 200 class respopnse")
   }
-  await binVersionCheck(bin.path(), `~${version}`, { args: ['version'] });
-  return bin;
+  await mkdir(dirname(filename), { recursive: true });
+  await new Promise((resolve, reject) => {
+    const bin = createWriteStream(filename, { autoClose: true });
+    bin.on('error', reject);
+    bin.on('close', resolve);
+    bin.on('open', () => {
+      res.pipe(bin);
+      res.on('error', (e) => {
+        reject(e)
+        bin.close();
+      });
+      res.on('end', () => bin.close())
+    });
+  });
+  if (platform() != 'win32') {
+    await chmod(filename, "700");
+  }
+}
+
+async function fetchCheckBin() {
+  try {
+    await stat(filename);
+  } catch {
+    await fetchBin().catch((e) => {
+      console.log(e);
+      throw e;
+    });
+  }
+  await binVersionCheck(filename, `~${version}`, { args: ['version'] });
+
 }
 
 async function tryRemove(e: unknown): Promise<void> {
-  await rmdirP(dest, { recursive: true });
+  await rm(dest, { recursive: true });
   throw e;
 }
 
-export async function stucco(): Promise<BinWrapper> {
-  const bin = await retry(() => fetchCheckBin().catch(tryRemove), { retries: 2, minTimeout: 10 });
-  if (!bin) {
-    throw new Error('could not find stucco binary');
-  }
-  return bin;
+export async function stucco(): Promise<string> {
+  await retry(() => fetchCheckBin().catch(tryRemove), { retries: 2, minTimeout: 10 });
+  return filename;
 }
